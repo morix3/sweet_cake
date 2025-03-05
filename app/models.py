d3f9e1295530100321e345287d33f34a76794cf0
Morix <morixmart@mail.ru>
2025-03-05 17:26:03 +0400

    init commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+
