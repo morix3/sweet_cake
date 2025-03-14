62c2fd7197342275c803a43ea3f18701065a3b19
Morix <morixmart@mail.ru>
2025-03-14 10:16:53 +0400

    add templates and routes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,31 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Database connected successfully!"
+    return render_template('index.html')
+
+
+@app.route('/cake-details')
+def cake_details():
+    return render_template('cake-details.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
